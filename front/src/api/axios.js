b03c20512771dbb86bde2d35d3b41a4dba47efc9
Maeez Pathan <mazu262002@gmail.com>
2026-03-13 16:48:50 +0530

    connect frontend to production api

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
+axios.create({
+  baseURL: "https://jammat-system.onrender.com"
+})
 
 export default API;
